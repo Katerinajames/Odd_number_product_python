94c02a2c4851d4b4e0adbb2acfceee1a6021bfbd
Katerina Paparizou <76427845+Katerinajames@users.noreply.github.com>
2024-10-24 10:13:12 +0300

    Add files via upload

---
+++ calculate_product_of_odd_integers.py
@@ -0,0 +1,10 @@
+
+product=1
+print("ODD NUMBER ARRAY")
+for i in range(1,16):
+	if i%2!=0:
+	 print("%d\n"%(i))
+	 product=product*i
+	
+
+print("The product of the odd array elements is %d"%(product))
